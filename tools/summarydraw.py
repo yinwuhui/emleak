730cfe7ac64b1dccad4df33af66048cc54f738ea
yinwuhui <1297167286@qq.com>
2023-03-01 17:02:45 +0800

    mem unit change to KB.

--- tools/summarydraw.py
+++ tools/summarydraw.py
@@ -7,14 +7,14 @@ with open('mleaksummary.csv', 'r') as csvfile:
 
 # 读取数据
 labels = [row[0] for row in data]
-total = [int(row[1]) for row in data]
+total = [int(row[1])/(1024) for row in data]
 times = [int(row[2]) for row in data]
 
 # 创建柱状图
 bar_chart = pygal.Bar()
 bar_chart.title = 'All stack malloc memry total and times'
 bar_chart.x_labels = labels
-bar_chart.add('total', total)
+bar_chart.add('total (KB)', total)
 bar_chart.add('times', times)
 
 # 输出图表
